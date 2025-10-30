bdf6efa64593933db75b290584bb9baafaf32dc4
Damian Flett <damian.flett@outlook.com>
2025-10-30 15:21:53 +1000

    completes driver script for full pipeline, writes make_predictions in predict.py

--- recognition/project_1_oasis_segmentation_s4744705/driver.py
+++ recognition/project_1_oasis_segmentation_s4744705/driver.py
@@ -1,15 +1,47 @@
 """
 In essence, does the same as  running train.py to get the model, and then passes that to predict functions.
 """
-from train import train, test, create_plots, dice_score
-from torch.utils.data import DataLoader, Dataset
+from train import train, test
+from predict import make_predictions
 from dataset import get_datasets_and_data_loaders
 import torch
 import argparse
+from modules import ImprovedUnet
 
 
 def main():
-    pass
+    """
+    Completes the full train, test, predict pipeline. Note that if not train_new, new plots will not be created as those are created during the training process.
+    Also worth noting that if the full training pipeline is completed, testing and prediction will be done on the model that had the highest dice score during training, 
+    but otherwise will be done on the model after every epoch is run. 
+    """
+    parser = argparse.ArgumentParser(description = "Train and Test the model")
+    parser.add_argument("--batch-size", type = int, default = 2, help = "Batch Size used in training")
+    parser.add_argument("--epochs", type = int, default = 20, help = "Number of Epochs to train for")
+    parser.add_argument("--lr", type = float, default = 1e-4, help = "Learning Rate used in training")
+    parser.add_argument("--dropout-prob", type = float, default = 0.3, help = "Dropout probability in context modules")
+    parser.add_argument("--plot", type = bool, default = True, help = "Decides whether to make and save plots or not" )
+    parser.add_argument("--dir", type = str, default = "images/", help = "Where to save images for the report" )
+    parser.add_argument("--seed", type = int, default = 1, help = "The seed to use when choosing what to make predictions on" )
+    parser.add_argument("--train_new", type = bool, default = False, help = "True if you want to train the model, False if you wan tto use the model saved in best_model.pth" )
+    args = parser.parse_args()
+    batch_size = args.batch_size
+    epochs = args.epochs
+    lr = args.lr
+    dropout_prob = args.dropout_prob
+    plot = args.plot
+    dir = args.dir
+    seed = args.seed
+    train_new = args.train_new
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    _, train_loader, test_set, test_loader, _, validation_loader = get_datasets_and_data_loaders(batch_size)
+    if train_new:
+        model = ImprovedUnet(dropout_prob=dropout_prob).to(device)
+        train(model, train_loader, validation_loader, epochs = epochs, lr = lr, batch_size=batch_size, device = device, plot = plot)
+    else:
+        model = torch.load("best_model.pth")
+    test(model, test_loader, test_set, device = device)
+    make_predictions(model, dir = dir, seed = seed, device = device)
 
 if __name__ == "__main__":
     main()
--- recognition/project_1_oasis_segmentation_s4744705/predict.py
+++ recognition/project_1_oasis_segmentation_s4744705/predict.py
@@ -1,15 +1,33 @@
+# Only to be used as a module!
 from train import train, test, create_plots, dice_score
 from torch.utils.data import DataLoader, Dataset
 from dataset import get_datasets_and_data_loaders
 import torch
 import torch.nn as nn
 from modules import ImprovedUnet
+import random
+import matplotlib.pyplot as plt
 
-def make_predictions(model: ImprovedUnet, dir: str = "images/", seed: int = 1):
+def make_predictions(model: ImprovedUnet, dir: str = "images/", seed: int = 1, device = None) -> None:
     """
     Makes and saves to the specified directory predictions on a random susbet of the test, training, and validation sets
     model: The model to make predictions with
     dir: Directory to save predicted figures to 
     seed: The random seed with which to choose these subset
+    device: Device to make predictions on
     """
+    train_dataset, _, test_dataset, _, validation_dataset, _ = get_datasets_and_data_loaders(1)
+
+    random.seed(seed)
+    samples_train_indices = random.sample(range(len(train_dataset)), 4)
+    samples_test_indices = random.sample(range(len(test_dataset)), 4)
+    samples_validation_indices = random.sample(range(len(validation_dataset)),4)
+
+    model.eval()  
+    save_predictions(model, train_dataset, samples_train_indices, "training_preds_sample.png", dir, seed, device)
+    save_predictions(model, test_dataset, samples_test_indices, "test_preds_sample.png", dir, seed, device)
+    save_predictions(model, validation_dataset, samples_validation_indices, "validation_preds_sample.png", dir, seed, device)
+
+
+def save_predictions(model: ImprovedUnet, dataset: Dataset, indices: list[int], filename: str, dir: str = "images/", seed: int = 1, device = None) -> None:
     pass
